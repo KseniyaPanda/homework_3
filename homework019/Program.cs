bf9b7c04e22937b3eda07ea739aea5be29bbf02f
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-01 23:43:36 +0300

    решение 19 задачи

--- homework019/Program.cs
+++ homework019/Program.cs
@@ -7,3 +7,35 @@
 Палиндром, перевертень — число, буквосочетание, слово или текст, 
 одинаково читающееся в обоих направлениях.
 */
+
+int GetNumber(string message)
+{
+   int result = 0;
+   bool isCorrect = false;
+   while (!isCorrect)
+   {
+      Console.WriteLine(message);
+
+      if (int.TryParse(Console.ReadLine(), out result) && result >= 10000 && result <= 99999)
+      {
+         isCorrect = true;
+      }
+      else
+      {
+         Console.WriteLine("Введите корректное число");
+      }
+   }
+   return result;
+}
+
+int number = GetNumber("Введите пятизначное число");
+
+string numbers = number.ToString();
+char[] numRev = numbers.ToCharArray();
+Array.Reverse(numRev);
+string newnumber = new String(numRev);
+
+if (numbers == newnumber)
+   Console.WriteLine($"число {number} палиндром");
+else
+   Console.WriteLine("число не палиндром");
